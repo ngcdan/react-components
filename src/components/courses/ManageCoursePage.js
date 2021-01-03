a4321d1a74db6857c6231a2f221aafa10dcb3df1
LeNgocDan <Linuss1908@gmail.com>
2021-01-03 10:07:17 +0700

    fix code

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,10 +1,15 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {connect} from 'react-redux';
 import {loadCourses} from '../../redux/actions/courseActions';
 import {loadAuthors} from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
+import CourseForm from './CourseForm';
+import {newCourse} from '../../../tools/mockData';
 
-function CoursesPage({courses, authors, loadAuthors, loadCourses}) {
+function CoursesPage({courses, authors, loadAuthors, loadCourses, ...props}) {
+  const [course, setCourse] = useState({...props.course});
+  const [error, setError] = useState({});
+
   useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch(error => {
@@ -18,14 +23,19 @@ function CoursesPage({courses, authors, loadAuthors, loadCourses}) {
     }
   }, [])
 
-  return (
-    <>
-      <h2>Courses</h2>
-    </>
-  );
+  function handleChange(event) {
+    const {name, value} = event.target;
+    setCourse(prevCourse => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value
+    }));
+  }
+
+  return (<CourseForm course={course} error={error} authors={authors}onChange={handleChange} />);
 }
 
 CoursesPage.propTypes = {
+  course: PropTypes.object.isRequired,
   courses: PropTypes.array.isRequired,
   authors: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
@@ -39,6 +49,7 @@ const mapDispatchToProp = {
 
 function mapStateToProps(state) {
   return {
+    course: newCourse,
     courses: state.courses,
     authors: state.authors
   };
